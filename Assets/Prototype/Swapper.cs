a24cc74593baf92043ec96a39efabf2b5d5f5f64
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-09-27 22:44:44 +0200

    Raycast selection ok, problem with clicks

--- Assets/Prototype/Swapper.cs
+++ Assets/Prototype/Swapper.cs
@@ -7,41 +7,102 @@ public class Swapper : MonoBehaviour
     public ViewModeSwap viewModeSwap;
     private Transform _selection;
     public Sprite notSelectedSprite;
+    public Sprite hoveredSprite;
     public Sprite selectedSprite;
 
+    public int selectionCounter;
+    public Transform selectedPaint1;
+    public Transform selectedPaint2;
+
+    private void Start()
+    {
+        selectionCounter = 0;
+    }
 
 
     void FixedUpdate()
     {
 
-        if (_selection != null)
+        if (selectionCounter == 2)
         {
-            var selectionRenderer = _selection.GetComponent<SpriteRenderer>();
-            selectionRenderer.sprite = notSelectedSprite;
-            _selection = null;
+            Swap();
         }
 
-        int layerMask = 1 << 8;
-        Vector2 cubeRay = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D cubeHit = Physics2D.Raycast(cubeRay, Vector2.zero, layerMask);
-        var selection = cubeHit.transform;
 
-        if (cubeHit && cubeHit.collider.CompareTag("Painting"))
+        if (viewModeSwap.fullView)
         {
-            Debug.Log("We hit " + cubeHit.collider.name);
-            var selectionRenderer = selection.GetComponent<SpriteRenderer>();
 
-            if (Input.GetMouseButtonDown(0))
+            if (_selection != null)
             {
-                Debug.Log("Funge");
+                var selectionRenderer = _selection.GetComponent<SpriteRenderer>();
+                selectionRenderer.sprite = notSelectedSprite;
+                _selection = null;
             }
-            if (selectionRenderer != null)
+
+            int layerMask = 1 << 8 | 1 << 9;
+            Vector2 cubeRay = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D cubeHit = Physics2D.Raycast(cubeRay, Vector2.zero, layerMask);
+            var selection = cubeHit.transform;
+
+            if (cubeHit && cubeHit.collider.CompareTag("SelectedPainting"))
             {
-                selectionRenderer.sprite = selectedSprite;
+                Debug.Log("We hit " + cubeHit.collider.name);
+                if (Input.GetMouseButtonDown(0) && selectionCounter == 1)
+                {
+                    selectionCounter--;
+                    selectedPaint1.root.tag = "Painting";
+                    selectedPaint1.GetChild(0).gameObject.SetActive(false);
+                }
             }
+
+            else if (cubeHit && cubeHit.collider.CompareTag("Painting"))
+            {
+                Debug.Log("We hit " + cubeHit.collider.name);
+                var selectionRenderer = selection.GetComponent<SpriteRenderer>();
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (selectionCounter == 0)
+                    {
+                        Debug.Log("Funge");
+                        cubeHit.collider.transform.root.tag = "SelectedPainting";
+                        cubeHit.collider.transform.GetChild(0).gameObject.SetActive(true);
+                        selectionCounter++;
+                        selectedPaint1 = cubeHit.transform;
+
+                    }
+                    else if (selectionCounter == 1)
+                    {
+                        Debug.Log("Funge");
+                        cubeHit.collider.transform.root.tag = "SelectedPainting";
+                        cubeHit.collider.transform.GetChild(0).gameObject.SetActive(true);
+                        selectionCounter++;
+                        selectedPaint2 = cubeHit.transform;
+                    }
+                }
+
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.sprite = hoveredSprite;
+                }
+
+
+            }
+            _selection = selection;
 
 
         }
-        _selection = selection;
+    }
+    public void Swap()
+    {
+        selectedPaint1.GetChild(0).gameObject.SetActive(false);
+        selectedPaint2.GetChild(0).gameObject.SetActive(false);
+
+        Vector3 tempPosition = selectedPaint1.transform.position;
+        selectedPaint1.transform.position = selectedPaint2.transform.position;
+        selectedPaint2.transform.position = tempPosition;
+        selectionCounter = 0;
+        selectedPaint1.root.tag = "Painting";
+        selectedPaint2.root.tag = "Painting";
     }
 }
