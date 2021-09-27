cd5e0102f9eee74a1ca69776761ce835e98281e1
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-09-27 19:09:37 +0200

    Added placeholder and cameras

---
+++ Assets/Prototype/Swapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swapper : MonoBehaviour
+{
+    public float distance = 50f;
+
+    void FixedUpdate()
+    {
+        //if mouse button (left hand side) pressed instantiate a raycast
+        if (Input.GetMouseButtonDown(0))
+        {
+            //create a ray cast and set it to the mouses cursor position in game
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, distance))
+            {
+                //draw invisible ray cast/vector
+                Debug.DrawLine(ray.origin, hit.point);
+                //log hit area to the console
+                Debug.Log(hit.point);
+
+            }
+        }
+    }
+}
