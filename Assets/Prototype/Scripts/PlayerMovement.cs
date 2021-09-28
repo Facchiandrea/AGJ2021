936f09882ae2d4492c1f0e495ee31822c2371e80
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-09-28 20:04:31 +0200

    Test proporzioni

---
+++ Assets/Prototype/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float movementSpeed = 2f;
+    float horizontalMovement = 0f;
+    public ViewModeSwap viewModeSwap;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        if (viewModeSwap.fullView == false)
+        {
+            horizontalMovement = Input.GetAxis("Horizontal");
+            if (horizontalMovement > 0f)
+            {
+                rb.velocity = new Vector2(horizontalMovement * movementSpeed, rb.velocity.y);
+            }
+            if (horizontalMovement < 0f)
+            {
+                rb.velocity = new Vector2(horizontalMovement * movementSpeed, rb.velocity.y);
+            }
+        }
+
+    }
+}
