5d84429e57e6f580d2be966561b8480dbc460e75
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-10-04 18:32:56 +0200

    Menu placeholder e audio impostato

--- Assets/Prototype/Scripts/Menu/MenuManager.cs
+++ Assets/Prototype/Scripts/Menu/MenuManager.cs
@@ -7,20 +7,68 @@ public class MenuManager : MonoBehaviour
     public GameObject startingScreen;
     public GameObject audioScreen;
     public GameObject creditScreen;
+    public bool inAudioOptions;
+    public bool inCredits;
 
+    private void Start()
+    {
+        AudioManager.instance.FadeIn("MenuMusic");
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(1))
+        {
+            if (inAudioOptions)
+            {
+                inAudioOptions = false;
+                inCredits = false;
+                startingScreen.SetActive(true);
+                audioScreen.SetActive(false);
+                creditScreen.SetActive(false);
+
+            }
+            else if (inCredits)
+            {
+                inAudioOptions = false;
+                inCredits = false;
+                startingScreen.SetActive(true);
+                audioScreen.SetActive(false);
+                creditScreen.SetActive(false);
+
+            }
+            else
+            {
+                Quitting();
+            }
+        }
+    }
 
     public void Play()
     {
         SceneManager.LoadScene(1);
     }
 
+    public void Quitting()
+    {
+        Application.Quit();
+    }
     public void AudioButton()
     {
+        inAudioOptions = true;
+        inCredits = false;
 
+        startingScreen.SetActive(false);
+        audioScreen.SetActive(true);
+        creditScreen.SetActive(false);
     }
     public void CreditsButton()
     {
+        inAudioOptions = false;
+        inCredits = true;
 
+        startingScreen.SetActive(false);
+        audioScreen.SetActive(false);
+        creditScreen.SetActive(true);
     }
 
 }
