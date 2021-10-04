2011fb25b76889c2c55e23732cd396d033940d04
Andrea Facchinetti <facchiandrea1510@gmail.com>
2021-10-04 17:50:46 +0200

    Menu wip

---
+++ Assets/Prototype/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    public GameObject startingScreen;
+    public GameObject audioScreen;
+    public GameObject creditScreen;
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void AudioButton()
+    {
+
+    }
+    public void CreditsButton()
+    {
+
+    }
+
+}
