5b373f2937a60eb18635b611b745e65b2d41e4fc
Andrea Facchinetti <facchiandrea1510@gmail.com>
2022-09-15 17:11:10 +0200

    Added headphones msg

---
+++ Assets/HeadphonesMSGScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadphonesMSGScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DeactivatePanel", 4f);
+    }
+
+    public void DeactivatePanel()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+}
